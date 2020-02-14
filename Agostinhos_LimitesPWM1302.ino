e60569a40472df7cc0410e259d07529f0d24e4ba
Briza0407 <37940467+Briza0407@users.noreply.github.com>
2020-02-13 22:00:13 -0300

    Add files via upload

---
+++ Agostinhos_LimitesPWM1302.ino
@@ -0,0 +1,233 @@
+// Este código tem como objetivo ser a continuação do ReadLine manual
+// utilizando a ideia da linha ser branca e o fundo ser preto.
+//
+
+// PWM         40    -----       60     -----       80     -----       100   -----    120        -----    140       -----   160      -----    200    -----    210
+// Kp:      0.05     -----     0.08     -----     0.013    -----     0.2     -----    0.22       -----    0.21      -----   0    -----    0.1    -----
+// Ki:          0    -----        0     -----     0        -----             -----     -0.00005  -----    -0.0000  -----            -----           -----
+// Kd:       0.1     -----      0.3     -----      0.45    -----      0.64   -----    1.15       -----    1.18      -----   0     -----    0.35   -----
+// Tensão:   7.67    -----     7.61     -----     7.55     -----     7.55    -----               -----    7.49      -----   0     -----    7.55   -----
+
+#include <QTRSensors.h>
+
+#define SaidaA 9
+#define SaidaB 3
+
+//Lado direito
+#define AIN2 6 // Quando em HIGH, roda direita anda para frente
+#define AIN1 5
+
+//Lado Esquerdo
+#define BIN1 4 // Quando em HIGH, roda esquerda anda para frente
+#define BIN2 2
+
+double Kp = 0.27, Kd = 1.18, Ki = 0; // Variáveis que são modificadas no PID
+int PWM = 160, Valor = 0; // valor da força do motor em linha reta
+
+double erro, p, d, erroAnterior = 0, i, integral = 0, Turn = 0; //Área PID
+double MotorA, MotorB; // Tensao enviada pelo PID
+
+double leitura1 = 0, leitura2 = 0; // Método usado com o readCalibrated
+
+int position_line = 0; // Método com o Read Line;
+
+int ExcessoA, ExcessoB;
+
+int contador = 0, acionador = 0; // Borda
+
+QTRSensorsRC borda ((unsigned char[]) {A3}, 1); //Sensor de borda
+QTRSensorsAnalog qtra  ((unsigned char[]) {  A2, A1, A0, A7, A6, A4}, 6); //Sensores frontais
+
+unsigned int sensor_values[6];
+unsigned int BordaValor[1];
+
+void setup() {
+  Serial.begin(9600); // Comunicacao com o Serial
+
+  pinMode(A0,      INPUT);
+  pinMode(A1,      INPUT);
+  pinMode(A2,      INPUT);
+  pinMode(A3,      INPUT);
+  pinMode(A4,      INPUT);
+  pinMode(A5,      INPUT);
+  pinMode(A6,      INPUT);
+  pinMode(AIN1,   OUTPUT);
+  pinMode(AIN2,   OUTPUT);
+  pinMode(BIN1,   OUTPUT);
+  pinMode(BIN2,   OUTPUT);
+  pinMode(MotorA, OUTPUT);
+  pinMode(MotorB, OUTPUT);
+
+  //----> Calibração do sensor de borda <----
+
+  Serial.println("Iniciando calibração do sensor de borda...");
+
+  for (int i = 0; i < 100; i++)
+  {
+    borda.calibrate();
+    delay(5);
+  }
+
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+
+  //----> Calibração dos Sensores frontais <----
+
+  for (int i = 0; i < 150; i++)
+  {
+    qtra.calibrate();
+    delay(5);
+  }
+
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+}
+
+void loop() {
+  digitalWrite(13,LOW);
+  borda.readCalibrated(BordaValor);
+
+  position_line = qtra.readLine(sensor_values, QTR_EMITTERS_ON,1);
+  erro = position_line - 2500;
+
+//  Serial.println(position_line);
+
+  if (position_line <= 500)
+  {
+    while (position_line < 2500)
+    {
+      digitalWrite(13, HIGH);
+      digitalWrite(AIN1, LOW);
+      digitalWrite(AIN2, HIGH);
+      digitalWrite(BIN2, HIGH);
+      digitalWrite(BIN1, LOW);
+
+      analogWrite(SaidaA, 70);
+      analogWrite(SaidaB, 70);
+
+      position_line = qtra.readLine(sensor_values, QTR_EMITTERS_ON);
+    }
+  }
+  else if (position_line >= 4500)
+  {
+    while (position_line > 2500)
+    {
+      digitalWrite(13, HIGH);
+      digitalWrite(AIN1, HIGH);
+      digitalWrite(AIN2, LOW);
+      digitalWrite(BIN2, LOW);
+      digitalWrite(BIN1, HIGH);
+
+      analogWrite(SaidaA, 70);
+      analogWrite(SaidaB, 70);
+
+      position_line = qtra.readLine(sensor_values, QTR_EMITTERS_ON);
+    }
+  }
+
+  //----> Area do Sensor de Borda <----
+
+  if ((BordaValor[0] < 300) && (acionador == 0))
+  {
+    contador++;
+    acionador = 1;
+  }
+
+  if ((BordaValor[0] > 700) && (acionador == 1))
+  {
+    acionador = 0;
+  }
+
+  while (contador >= 3)
+  {
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, HIGH);
+    digitalWrite(BIN2, HIGH);
+    digitalWrite(BIN1, HIGH);
+
+    analogWrite(SaidaA, LOW);
+    analogWrite(SaidaB, LOW);
+
+    delay (1000);
+
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, LOW);
+    digitalWrite(BIN2, LOW);
+    digitalWrite(BIN1, LOW);
+  }
+
+
+  //--------------->AREA DO PID<---------------
+
+  p = erro * Kp; // Proporcao
+
+  integral += erro; // Integral
+  i = Ki * integral;
+
+  d = Kd * (erro - erroAnterior); // Derivada
+  erroAnterior = erro;
+
+  Turn = p + i + d;
+
+  MotorA = PWM - Turn;
+  MotorB = PWM + Turn;
+
+  //--------------->AREA DO SENTIDO DAS RODAS<---------------
+
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  digitalWrite(BIN2, LOW);
+  digitalWrite(BIN1, HIGH);
+
+  if (MotorA < 0) // Giro para a direita
+  {
+    digitalWrite(AIN1, HIGH);
+    digitalWrite(AIN2, LOW);
+  }
+  if (MotorB < 0) // Giro para a esquerda
+  {
+    digitalWrite(BIN2, HIGH);
+    digitalWrite(BIN1, LOW);
+  }
+
+
+
+  if (MotorA > 260)
+  {
+    ExcessoB = (abs(MotorA) - 250) / 2;
+    MotorA = 250;
+    MotorB -= ExcessoB;
+  }
+
+  else if (MotorB > 260)
+  {
+    ExcessoA = (abs(MotorB) - 250) / 2;
+    MotorB = 250;
+    MotorA -= ExcessoA;
+  }
+
+  if (MotorA < -260)
+  {
+    ExcessoB = (abs(MotorA) - 250) / 2;
+    MotorA = 250;
+    MotorB += ExcessoB;
+  }
+
+  else if (MotorB < -260)
+  {
+    ExcessoA = (abs(MotorB) - 250) / 2;
+    MotorB = 250;
+    MotorA += ExcessoA;
+  }
+
+
+  analogWrite(SaidaA, MotorA);
+  analogWrite(SaidaB, MotorB);
+
+}
